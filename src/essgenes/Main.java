887d9785ca57109b06c89f7e6175c5429a123ae3
sina <sina@fb34ffc9-a880-4907-b33b-9e3314847d79>
2013-10-08 03:08:22 +0000

    UIManager Change

--- src/essgenes/Main.java
+++ src/essgenes/Main.java
@@ -18,7 +18,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 		} 
 		catch (UnsupportedLookAndFeelException e) {
 			
