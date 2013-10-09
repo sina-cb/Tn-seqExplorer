09eda2df5dc94f3e0fdc72e85a58d1e6e186bd7d
sina <sina@fb34ffc9-a880-4907-b33b-9e3314847d79>
2013-10-09 18:08:47 +0000

    Testing GUI problem with linux

--- src/GUI/RemoteInstall.java
+++ src/GUI/RemoteInstall.java
@@ -30,6 +30,7 @@ public class RemoteInstall extends JFrame {
 	private JTextField txtCdBwaa;
 	private JTextField txtMake;
 	private JTextField txtusrsbinalternativesinstallusrbinbwa;
+	private JTextField txtWgetFileurl;
 
 	/**
 	 * Create the frame.
@@ -38,7 +39,7 @@ public class RemoteInstall extends JFrame {
 		setTitle("Do it manually");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 611, 487);
+		setBounds(100, 100, 818, 487);
 		this.setLocationRelativeTo(parentFrame);
 		contentPane = new JPanel();
 		contentPane.setForeground(Color.BLACK);
@@ -47,15 +48,15 @@ public class RemoteInstall extends JFrame {
 		contentPane.setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 11, 581, 437);
+		tabbedPane.setBounds(10, 11, 792, 437);
 		contentPane.add(tabbedPane);
 		
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Install BWA on your remote machine", null, panel, null);
 		panel.setLayout(null);
 		
-		JLabel lblDownloadBwa = new JLabel("2. Download BWA source code from ");
-		lblDownloadBwa.setBounds(20, 61, 185, 14);
+		JLabel lblDownloadBwa = new JLabel("2. Download BWA source code from  their website");
+		lblDownloadBwa.setBounds(20, 61, 314, 14);
 		panel.add(lblDownloadBwa);
 		
 		JLabel label_1 = new JLabel("BWA website");
@@ -73,15 +74,15 @@ public class RemoteInstall extends JFrame {
 			}
 		});
 		label_1.setForeground(Color.BLUE);
-		label_1.setBounds(215, 61, 78, 14);
+		label_1.setBounds(344, 61, 78, 14);
 		panel.add(label_1);
 		
 		JLabel label_2 = new JLabel("Steps to install BWA on your remote machine:");
-		label_2.setBounds(10, 11, 219, 14);
+		label_2.setBounds(10, 11, 767, 14);
 		panel.add(label_2);
 		
 		JLabel lblConnectTo = new JLabel("1. Connect to your remote machine using SSH. ");
-		lblConnectTo.setBounds(20, 36, 245, 14);
+		lblConnectTo.setBounds(20, 36, 314, 14);
 		panel.add(lblConnectTo);
 		
 		JLabel lblyouCanUse = new JLabel("(You can use PuTTY as your SSH client)");
@@ -99,21 +100,21 @@ public class RemoteInstall extends JFrame {
 			}
 		});
 		lblyouCanUse.setForeground(Color.BLUE);
-		lblyouCanUse.setBounds(252, 36, 204, 14);
+		lblyouCanUse.setBounds(344, 36, 255, 14);
 		panel.add(lblyouCanUse);
 		
-		JLabel lblYouCan = new JLabel("- You can download the file using 'wget' command like this:  wget FileURL");
-		lblYouCan.setBounds(30, 86, 369, 14);
+		JLabel lblYouCan = new JLabel("- You can download the file using 'wget' command like this, ");
+		lblYouCan.setBounds(30, 86, 747, 14);
 		panel.add(lblYouCan);
 		
 		JLabel lblUncompressThe = new JLabel("3. Uncompress the downloaded file using the example command below:");
-		lblUncompressThe.setBounds(20, 111, 436, 14);
+		lblUncompressThe.setBounds(20, 111, 747, 14);
 		panel.add(lblUncompressThe);
 		
 		txtTar = new JTextField();
 		txtTar.setText("tar -jxf bwa-0.7.5a.tar.bz2");
 		txtTar.setEditable(false);
-		txtTar.setBounds(101, 136, 151, 20);
+		txtTar.setBounds(159, 136, 151, 20);
 		panel.add(txtTar);
 		txtTar.setColumns(10);
 		
@@ -133,7 +134,7 @@ public class RemoteInstall extends JFrame {
 		txtCdBwaa.setText("cd bwa-0.7.5a");
 		txtCdBwaa.setEditable(false);
 		txtCdBwaa.setColumns(10);
-		txtCdBwaa.setBounds(101, 189, 151, 20);
+		txtCdBwaa.setBounds(159, 189, 151, 20);
 		panel.add(txtCdBwaa);
 		
 		JLabel lblCompileThe = new JLabel("5. Compile the code using the command below:");
@@ -148,11 +149,11 @@ public class RemoteInstall extends JFrame {
 		txtMake.setText("make");
 		txtMake.setEditable(false);
 		txtMake.setColumns(10);
-		txtMake.setBounds(101, 242, 151, 20);
+		txtMake.setBounds(159, 242, 151, 20);
 		panel.add(txtMake);
 		
 		JLabel lblInstallThe = new JLabel("6. Install the application using the example command below:");
-		lblInstallThe.setBounds(20, 270, 379, 14);
+		lblInstallThe.setBounds(20, 270, 536, 14);
 		panel.add(lblInstallThe);
 		
 		JLabel label_4 = new JLabel("- Example:");
@@ -163,23 +164,35 @@ public class RemoteInstall extends JFrame {
 		txtusrsbinalternativesinstallusrbinbwa.setText("/usr/sbin/alternatives --install /usr/bin/bwa bwa (Uncompressed Directory) 20000");
 		txtusrsbinalternativesinstallusrbinbwa.setEditable(false);
 		txtusrsbinalternativesinstallusrbinbwa.setColumns(10);
-		txtusrsbinalternativesinstallusrbinbwa.setBounds(101, 295, 401, 20);
+		txtusrsbinalternativesinstallusrbinbwa.setBounds(159, 295, 401, 20);
 		panel.add(txtusrsbinalternativesinstallusrbinbwa);
 		
 		JLabel lblInTheExample = new JLabel("- In the example command above, you only have to replace the (Uncompressed Directory)");
-		lblInTheExample.setBounds(30, 323, 536, 14);
+		lblInTheExample.setBounds(30, 323, 633, 14);
 		panel.add(lblInTheExample);
 		
 		JLabel lblPathOfThe = new JLabel("with the complete path of the uncompressed folder");
-		lblPathOfThe.setBounds(40, 348, 270, 14);
+		lblPathOfThe.setBounds(40, 348, 466, 14);
 		panel.add(lblPathOfThe);
 		
 		JLabel lblBwaIs = new JLabel("7. BWA is installed");
-		lblBwaIs.setBounds(20, 373, 379, 14);
+		lblBwaIs.setBounds(20, 373, 436, 14);
 		panel.add(lblBwaIs);
+		
+		txtWgetFileurl = new JTextField();
+		txtWgetFileurl.setEditable(false);
+		txtWgetFileurl.setText("wget FileURL");
+		txtWgetFileurl.setBounds(344, 83, 86, 20);
+		panel.add(txtWgetFileurl);
+		txtWgetFileurl.setColumns(10);
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Run BWA on your remote machine", null, panel_1, null);
+		panel_1.setLayout(null);
+		
+		JLabel lblAfter = new JLabel("After ");
+		lblAfter.setBounds(10, 11, 46, 14);
+		panel_1.add(lblAfter);
 	}
 	
 	public void setParentFrame(JFrame parent){
